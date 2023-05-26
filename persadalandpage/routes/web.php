e385bcb120d4772a92b508df4c4d2f5f0c3aa11a
bloomycoder <134682846+bloomycoder@users.noreply.github.com>
2023-05-26 16:48:20 +0800

    Update web.php

--- persadalandpage/routes/web.php
+++ persadalandpage/routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('index');
+});
 
-Route::get('/',[HomeController::class,'index'])->name('home');
+//Route::get('/',[HomeController::class,'index'])->name('home');
 
 Route::get('tehpersada', function () {
     return view('tehpersada');
